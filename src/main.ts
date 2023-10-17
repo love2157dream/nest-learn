6280f6d46660c693850e9b4738d95181b203882c
lianglei <love2157dream@aliyun.com>
2023-10-17 14:21:33 +0800

    add: 增加服务启动日志

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
+  const PORT = 1417;
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const logger: Logger = new Logger();
+  await app.listen(PORT, () => {
+    logger.log(`服务已经启动，接口请访问http://localhost:${PORT}`);
+  });
 }
 bootstrap();
